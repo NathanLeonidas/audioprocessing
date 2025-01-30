de29a05cf1aca7345d3500817dd70c08d81792e5
Lewy Nathan <nathan.lewy@student-cs.fr>
2025-01-30 15:30:01 +0100

    deleted venv added reqs

--- Partie_3/3_freq_dominantes.py
+++ Partie_3/3_freq_dominantes.py
@@ -2,23 +2,29 @@ import numpy as np
 import librosa
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
-
+import os
 # Charger le fichier audio
-file_path = "audio_files/myson.wav"  # Ajuste ce chemin si nécessaire
+
+# Construire le chemin relatif vers le fichier audio
+script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Charger le fichier audio avec soundfile
+file = 'myson.wav'
+file_path = os.path.join(script_dir, 'audio_files', file)
+
 y, sr = librosa.load(file_path, sr=None)
 
 # Calcul du spectre de puissance
-spectrum = np.abs(librosa.stft(y, n_fft=4000, hop_length=80))
-frequencies = librosa.fft_frequencies(sr=sr, n_fft=len(spectrum))
-power = np.mean(spectrum, axis=1)
+spectrum = np.abs(np.fft.rfft(y))
+frequencies = np.fft.rfftfreq(len(y), d=1/sr)
 
 # Détection des pics
-peaks, _ = find_peaks(power, height=np.max(power) * 0.1)
+peaks, _ = find_peaks(spectrum, height=np.max(spectrum) * 0.1)
 
 # Affichage du spectre avec pics dominants
 plt.figure(figsize=(12, 6))
-plt.plot(frequencies[:400], power[:400], label="Spectre du signal")
-plt.scatter(frequencies[peaks], power[peaks], color='red', label="Pics dominants")
+plt.plot(frequencies, spectrum, label="Spectre du signal")
+plt.scatter(frequencies[peaks], spectrum[peaks], color='red', label="Pics dominants")
 plt.xlabel("Fréquence (Hz)")
 plt.ylabel("Amplitude")
 plt.title("Fréquences dominantes du signal")
